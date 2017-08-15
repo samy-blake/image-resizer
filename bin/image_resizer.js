7173d524012fa56dc34afd3d79a6ea1a431fc631
David Maciejewski <macx@macx.de>
2017-08-15 16:10:10 +0200

    use forked image resizer instead of the origin

--- bin/image_resizer.js
+++ bin/image_resizer.js
@@ -60,7 +60,7 @@ function createApplicationAt(dir){
     dependencies: {
       'chalk': pkg.dependencies.chalk,
       'express': pkg.dependencies.express,
-      'image-resizer': 'tripviss/image-resizer#v' + pkg.version,
+      'image-resizer': 'macx/image-resizer#v' + pkg.version,
       'lodash': pkg.dependencies.lodash,
       'sharp': pkg.dependencies.sharp
     },
